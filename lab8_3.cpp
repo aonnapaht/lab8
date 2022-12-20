f1db489367459c5084aba8ac3bdcdfa8e88bb22e
aonnapaht <napaht_d@cmu.ac.th>
2022-12-20 17:04:16 +0700

    lab8_3

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -2,18 +2,30 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+    char ans; 
+    if(x == 'A'){
+        x='Z';
+        return x;
+    }
+
+    if('A' <= x and x <= 'Z'){
+        x = x-1;
+        return x;
+    }
+    else{
+        ans='0';
+        return ans;
+    }
+
 }
 
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+int main (){
+    cout << before('A');
+    cout << before('B');
+    cout << before('P');
+    cout << before('T');
+    cout << before('Z');
+    cout << before('a');
+    cout << before('0');
+    cout << before('c');
 }
